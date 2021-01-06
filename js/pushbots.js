559cc90556154ce80a1610da2e15789f584c7a8c
Adrien V <72559937+AdrienVirieux@users.noreply.github.com>
2021-01-07 00:33:01 +0100

    Update pushbots.js

--- js/pushbots.js
+++ js/pushbots.js
@@ -2,7 +2,7 @@
 var PB = PB || {};PB.q = PB.q || [];PB.events = PB.events || [];
 
 //PushBots ApplicationId (required)
-PB.app_id = "5ff5117420d67541b6347c25";
+PB.app_id = "5ff64878a23a47362956ef33";
 //Your domain name, must be HTTPS or localhost  (required)
 PB.domain = "https://hungry-johnson-311663.netlify.app";
 //Update and uncomment it if you are using custom safari certificate for your app
