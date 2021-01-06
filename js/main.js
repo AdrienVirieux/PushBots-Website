7ba748574d88f69f78560dff70d418f51e0024b3
Adrien V <72559937+AdrienVirieux@users.noreply.github.com>
2021-01-06 04:42:41 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,32 +8,32 @@ function init(){
         funcPath = "sendNotification1()";
         document.getElementById("notif-button").setAttribute("onclick", funcPath);
     }
-    var button = document.getElementById('but1');
-    button.addEventListener('click', function1, true);
+    var button1 = document.getElementById('but1');
+    button1.addEventListener('click', function1, true);
 
     // Change
     function function2() {
         funcPath = "sendNotification2()";
         document.getElementById("notif-button").setAttribute("onclick", funcPath);
     }
-    var button = document.getElementById('but2');
-    button.addEventListener('click', function2, true);
+    var button2 = document.getElementById('but2');
+    button2.addEventListener('click', function2, true);
 
     // Change
     function function3() {
         funcPath = "sendNotification3()";
         document.getElementById("notif-button").setAttribute("onclick", funcPath);
     }
-    var button = document.getElementById('but3');
-    button.addEventListener('click', function3, true);
+    var button3 = document.getElementById('but3');
+    button3.addEventListener('click', function3, true);
 
     // Change
     function function4() {
         funcPath = "sendNotification4()";
         document.getElementById("notif-button").setAttribute("onclick", funcPath);
     }
-    var button = document.getElementById('but4');
-    button.addEventListener('click', function4, true);
+    var button4 = document.getElementById('but4');
+    button4.addEventListener('click', function4, true);
 
 };
 
